fbea10656af76949f3b25593d77b673f5a133bf3
Bedros <bedo.bgroup@abv.bg>
2019-08-02 12:49:04 +0300

    Added Exam

--- C# OOP/Exams/C# OOP Basics Exam Retake - 19 December 2018/Core/RestaurantController.cs
+++ C# OOP/Exams/C# OOP Basics Exam Retake - 19 December 2018/Core/RestaurantController.cs
@@ -1,248 +1,57 @@
-﻿using SoftUniRestaurant.Models.Foods.Contracts.Models;
-
-namespace SoftUniRestaurant.Core
+﻿namespace SoftUniRestaurant.Core
 {
-    using SoftUniRestaurant.Models.Drinks.Contracts;
-    using SoftUniRestaurant.Models.Drinks.Models;
-    using SoftUniRestaurant.Models.Foods.Contracts;
-    using SoftUniRestaurant.Models.Tables.Contracts;
-    using SoftUniRestaurant.Models.Tables.Models;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+    using System;
 
     public class RestaurantController
     {
-        private readonly List<IFood> menu;
-        private readonly List<IDrink> drinks;
-        private readonly List<ITable> tables;
-        private decimal income;
-
-        public RestaurantController()
-        {
-            this.menu = new List<IFood>();
-            this.drinks = new List<IDrink>();
-            this.tables = new List<ITable>();
-            this.income = 0;
-        }
-
         public string AddFood(string type, string name, decimal price)
         {
-            IFood food = null;
-
-            switch (type)
-            {
-                case "Dessert":
-                    food = new Dessert(name, price);
-                    break;
-
-                case "MainCourse":
-                    food = new MainCourse(name, price);
-                    break;
-
-                case "Salad":
-                    food = new Salad(name, price);
-                    break;
-
-                case "Soup":
-                    food = new Soup(name, price);
-                    break;
-
-                default:
-                    break;
-            }
-
-            string result = string.Empty;
-
-            if (food != null)
-            {
-                this.menu.Add(food);
-                result = string.Format(OutputMessages.FoodMade,
-                    name,
-                    food.GetType().Name,
-                    food.Price);
-            }
-
-            return result;
+            throw new NotImplementedException();
         }
 
         public string AddDrink(string type, string name, int servingSize, string brand)
         {
-            IDrink drink = null;
-
-            switch (type)
-            {
-                case "Alcohol":
-                    drink = new Alcohol(name, servingSize, brand);
-                    break;
-
-                case "FuzzyDrink":
-                    drink = new FuzzyDrink(name, servingSize, brand);
-                    break;
-
-                case "Juice":
-                    drink = new Juice(name, servingSize, brand);
-                    break;
-
-                case "Water":
-                    drink = new Water(name, servingSize, brand);
-                    break;
-
-                default:
-                    break;
-            }
-
-            string result = string.Empty;
-
-            if (drink != null)
-            {
-                this.drinks.Add(drink);
-                result = string.Format(OutputMessages.DrinkMade,
-                    name,
-                    drink.Brand);
-            }
-
-            return result;
+            throw new NotImplementedException();
         }
 
         public string AddTable(string type, int tableNumber, int capacity)
         {
-            ITable table = null;
-
-            switch (type)
-            {
-                case "Inside":
-                    table = new InsideTable(tableNumber, capacity);
-                    break;
-
-                case "Outside":
-                    table = new OutsideTable(tableNumber, capacity);
-                    break;
-
-                default:
-                    break;
-            }
-
-            string result = string.Empty;
-
-            if (table != null)
-            {
-                this.tables.Add(table);
-                result = string.Format(OutputMessages.TableMade, table.TableNumber);
-            }
-
-            return result;
+            throw new NotImplementedException();
         }
 
         public string ReserveTable(int numberOfPeople)
         {
-            ITable targetTable = this.tables
-                .FirstOrDefault(t => t.Capacity >= numberOfPeople &&
-                t.IsReserved == false);
-
-            if (targetTable == null)
-            {
-                return string.Format(OutputMessages.NoAvailableTable, numberOfPeople);
-            }
-            else
-            {
-                targetTable.Reserve(numberOfPeople);
-
-                return string.Format(OutputMessages.TableReserved,
-                    targetTable.TableNumber,
-                    targetTable.NumberOfPeople);
-            }
+            throw new NotImplementedException();
         }
 
         public string OrderFood(int tableNumber, string foodName)
         {
-            ITable targetTable = this.tables
-                .FirstOrDefault(t => t.TableNumber == tableNumber);
-
-            if (targetTable == null)
-            {
-                return string.Format(OutputMessages.CouldNotFindTable, tableNumber);
-            }
-
-            IFood targetFood = this.menu.FirstOrDefault(f => f.Name == foodName);
-
-            if (targetFood == null)
-            {
-                return string.Format(OutputMessages.NoFoodInMenu, foodName);
-            }
-
-            targetTable.OrderFood(targetFood);
-
-            return string.Format(OutputMessages.FoodOrdered, tableNumber, foodName);
+            throw new NotImplementedException();
         }
 
         public string OrderDrink(int tableNumber, string drinkName, string drinkBrand)
         {
-            ITable targetTable = this.tables
-                .FirstOrDefault(t => t.TableNumber == tableNumber);
-
-            if (targetTable == null)
-            {
-                return string.Format(OutputMessages.CouldNotFindTable, tableNumber);
-            }
-
-            IDrink targetDrink = this.drinks.FirstOrDefault(d => d.Name == drinkName &&
-                d.Brand == drinkBrand);
-
-            if (targetDrink == null)
-            {
-                return string.Format(OutputMessages.NoDrinkInMenu, drinkName, drinkBrand);
-            }
-
-            targetTable.OrderDrink(targetDrink);
-
-            return string.Format(OutputMessages.DrinkOrdered,
-                tableNumber,
-                drinkName,
-                drinkBrand);
+            throw new NotImplementedException();
         }
 
         public string LeaveTable(int tableNumber)
         {
-            ITable table = this.tables.FirstOrDefault(t => t.TableNumber == tableNumber);
-
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine(string.Format(OutputMessages.TableInformation, tableNumber));
-            sb.AppendLine(string.Format(OutputMessages.BillInformation, table.GetBill()));
-            this.income += table.GetBill();
-            table.Clear();
-
-            return sb.ToString().TrimEnd();
+            throw new NotImplementedException();
         }
 
         public string GetFreeTablesInfo()
         {
-            StringBuilder sb = new StringBuilder();
-
-            foreach (ITable table in this.tables.Where(t => t.IsReserved == false))
-            {
-                sb.AppendLine(table.GetFreeTableInfo());
-            }
-
-            return sb.ToString().TrimEnd();
+            throw new NotImplementedException();
         }
 
         public string GetOccupiedTablesInfo()
         {
-            StringBuilder sb = new StringBuilder();
-
-            foreach (ITable table in this.tables.Where(t => t.IsReserved == true))
-            {
-                sb.AppendLine(table.GetOccupiedTableInfo());
-            }
-
-            return sb.ToString().TrimEnd();
+            throw new NotImplementedException();
         }
 
         public string GetSummary()
         {
-            return string.Format(OutputMessages.TotalIncome, this.income);
+            throw new NotImplementedException();
         }
     }
 }
--- C# OOP/Exams/C# OOP Basics Exam Retake - 19 December 2018/StartUp.cs
+++ C# OOP/Exams/C# OOP Basics Exam Retake - 19 December 2018/StartUp.cs
@@ -1,65 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Runtime.ConstrainedExecution;
-using SoftUniRestaurant.Core;
-
-namespace SoftUniRestaurant
+﻿namespace SoftUniRestaurant
 {
     public class StartUp
     {
         public static void Main()
         {
-            RestaurantController restaurantController = new RestaurantController();
-            while (true)
-            {
-                var input = Console.ReadLine().Split(' ').ToArray();
-                string mainCommand = input[0];
-                if (mainCommand == "END")
-                {
-                    Console.WriteLine(restaurantController.GetSummary());
-                    break;
-                }
-
-                try
-                {
-                    switch (mainCommand)
-                    {
-                        case "AddFood":
-                            Console.WriteLine(restaurantController.AddFood(input[1], input[2], decimal.Parse(input[3])));
-                            break;
-                        case "AddDrink":
-                            Console.WriteLine(restaurantController.AddDrink(input[1], input[2], int.Parse(input[3]), input[4]));
-                            break;
-                        case "AddTable":
-                            Console.WriteLine(restaurantController.AddTable(input[1], int.Parse(input[2]), int.Parse(input[3])));
-                            break;
-                        case "ReserveTable":
-                            Console.WriteLine(restaurantController.ReserveTable(int.Parse(input[1])));
-                            break;
-                        case "OrderFood":
-                            Console.WriteLine(restaurantController.OrderFood(int.Parse(input[1]), input[2]));
-                            break;
-                        case "OrderDrink":
-                            Console.WriteLine(restaurantController.OrderDrink(int.Parse(input[1]), input[2], input[3]));
-                            break;
-                        case "LeaveTable":
-                            Console.WriteLine(restaurantController.LeaveTable(int.Parse(input[1])));
-                            break;
-                        case "GetFreeTablesInfo":
-                            Console.WriteLine(restaurantController.GetFreeTablesInfo());
-                            break;
-                        case "GetOccupiedTablesInfo":
-                            Console.WriteLine(restaurantController.GetOccupiedTablesInfo());
-                            break;
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-               
-            }
-            
         }
     }
 }
