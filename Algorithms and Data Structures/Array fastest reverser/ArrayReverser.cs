7d4ce2423352f79d499323b186b1ac0a9be5a5c8
Bedros <bedo.bgroup@abv.bg>
2019-08-30 15:05:56 +0300

    Added algorithms

---
+++ Algorithms and Data Structures/Array fastest reverser/ArrayReverser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace recursion
+{
+    class BinarySearch
+    {
+        static T[] Reverser<T>(T[] items, int leftIndex, int rightIndex, int item)
+        {
+            for (int i = 1; i <= items.Length / 2; i++)
+            {
+                var spare = items[i - 1];
+                items[i - 1] = items[items.Length - i];
+                items[items.Length - i] = spare;
+            }
+
+            return items;
+        }
+        
+        static void Main(string[] args)
+        {
+            var items = Console.ReadLine().Split(' ').ToArray();
+        }
+    }
+}
---
+++ Algorithms and Data Structures/Binary Search/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace recursion
+{
+    class BinarySearch
+    {
+        static int Search(List<int> items, int leftIndex, int rightIndex, int item)
+        {
+            if (rightIndex < leftIndex)
+            {
+                return -1;
+            }
+
+            int middle = leftIndex + (rightIndex - leftIndex) / 2;
+
+            if (items[middle] == item)
+            {
+                return middle;
+            }
+
+            if (items[middle] > item)
+            {
+                return Search(items, leftIndex, middle - 1, item);
+            }
+
+            return Search(items, middle + 1, rightIndex, item);
+        }
+        
+        static void Main(string[] args)
+        {
+            var items = new List<int> {1, 2, 5, 7, 8, 10, 14, 17, 20, 32, 56, 64};
+            int rightIndex = items.Count - 1;
+            int leftIndex = 0;
+            Console.WriteLine(Search(items, 0, items.Count - 1, 14));
+            
+        }
+    }
+}
