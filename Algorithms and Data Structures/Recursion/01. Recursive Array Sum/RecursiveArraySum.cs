f21cd3f795e47455b15928ab3ef77393d7e2575f
Bedros <bedo.bgroup@abv.bg>
2019-09-02 19:26:32 +0300

    fixed pages

---
+++ Algorithms and Data Structures/Recursion/01. Recursive Array Sum/RecursiveArraySum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace recursion
+{
+    class BinarySearch
+    {
+        static int[] numbers = new int[] {1, 2, 3, 4, 5};
+        static int Sum(int index)
+        {
+            if (index == numbers.Length)
+            {
+                return 0;
+            }
+
+            else
+            {
+                return numbers[index] + Sum(index + 1);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(0));
+        }
+    }
+}
---
+++ Algorithms and Data Structures/Recursion/02. Recursive Factorial/RecursiveFactorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace recursion
+{
+    class BinarySearch
+    {
+        static int FactorialSum(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * FactorialSum(number - 1);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(FactorialSum(number));
+        }
+    }
+}
