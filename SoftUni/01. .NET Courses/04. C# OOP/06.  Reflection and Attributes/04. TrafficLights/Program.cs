85bbea073d18f7f4c3351ce6c4ec4d4cec7befcf
Bedros <bedo.bgroup@abv.bg>
2019-09-15 15:05:14 +0300

    Added Exercises

---
+++ SoftUni/01. .NET Courses/04. C# OOP/06.  Reflection and Attributes/04. TrafficLights/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inferno_Infinity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] pattern = new string[] {"red", "green", "yellow"};
+            var inputColor = Console.ReadLine().Split(' ').Select(x => new TrafficLight(x)).ToList();
+            int iterations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < iterations; i++)
+            {
+                inputColor.ForEach(x => x.ChangeColor());
+                Console.WriteLine(string.Join(" ", inputColor));
+            }
+        }
+    }
+}
---
+++ SoftUni/01. .NET Courses/04. C# OOP/06.  Reflection and Attributes/04. TrafficLights/TrafficLight.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inferno_Infinity
+{
+    public class TrafficLight
+    {
+        private enum TrafficLights
+        {
+            Red = 0,
+            Green = 1,
+            Yellow = 2
+        }
+        private TrafficLights color;
+
+        public string Signal
+        {
+            get { return this.color.ToString(); }
+            set { TrafficLights.TryParse(value, out color); }
+        }
+        public TrafficLight(string signal)
+        {
+            this.Signal = signal;
+        }
+
+        public void ChangeColor()
+        {
+            var signals = typeof(TrafficLights).GetEnumValues();
+            this.color = (TrafficLights)signals.GetValue((int)(color + 1) % signals.Length);
+        }
+
+        public override string ToString()
+        {
+            return this.Signal.ToString();
+        }
+    }
+}
